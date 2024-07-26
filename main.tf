1abe312c970cb2648210691586563a1d1fac0b55
busti <oss@busti.cool>
2024-07-27 01:37:35 +0200

    wip

--- main.tf
+++ main.tf
@@ -15,35 +15,10 @@ resource "libvirt_network" "k8s" {
   name   = "k8s"
   mode   = "nat"
   domain = "k8s.local"
-  addresses = ["10.42.0.0/24"]
+  addresses = ["10.42.0.0/24", "fd00::1/64"]
 
   dns {
     enabled = true
-  }
-
-  xml {
-    # By default, DHCP range is the whole subnet.
-    # We will eventually want virtual IPs, so try to make space for them.
-    # XSLT (I have no idea what I'm doing),
-    # because of https://github.com/dmacvicar/terraform-provider-libvirt/issues/794
-    xslt = <<EOF
-      <?xml version="1.0" ?>
-      <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-        <xsl:output omit-xml-declaration="yes" indent="yes"/>
-        <xsl:template match="node()|@*">
-           <xsl:copy>
-             <xsl:apply-templates select="node()|@*"/>
-           </xsl:copy>
-        </xsl:template>
-
-        <xsl:template match="/network/ip/dhcp/range">
-          <xsl:copy>
-            <xsl:attribute name="start">10.42.0.100</xsl:attribute>
-            <xsl:apply-templates select="@*[not(local-name()='start')]|node()"/>
-          </xsl:copy>
-        </xsl:template>
-      </xsl:stylesheet>
-    EOF
   }
 }
 
